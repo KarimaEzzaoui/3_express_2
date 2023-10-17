ce3b96a369f3dc98a86bd78009f84c386d495b88
KarimaEzzaoui <140560159+KarimaEzzaoui@users.noreply.github.com>
2023-10-17 17:47:44 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,156 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const flash = require('express-flash');
+
+const app = express();
+const port = 3000;
+
+// Session --------------
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(
+  session({
+    secret: '123456789',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+
+// Passport initialization
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Authentication middleware --------------
+const authenticate = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect('/auth/login');
+  }
+};
+
+// Connection --------------
+mongoose.connect('mongodb://127.0.0.1:27017/testDB2').then(() => {
+  console.log('connected successfully');
+}).catch((error) => {
+  console.log(error);
+});
+
+// Models ------------------------------------
+const bookSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+});
+const Book = mongoose.model('Book', bookSchema);
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+const User = mongoose.model('User', userSchema);
+
+// Passport Local Strategy
+passport.use(new LocalStrategy(
+  async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }
+));
+
+// Passport Serialization
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+// Routers ---------------------------------------------
+const authRouter = express.Router();
+const booksRouter = express.Router();
+
+// End Points ------------------------------------------
+authRouter.get('/register', (req, res) => {
+  res.render('register', { messages: req.flash('error') });
+});
+
+authRouter.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    req.flash('error', 'Username already taken');
+    return res.redirect('/auth/register');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ username, password: hashedPassword });
+  await newUser.save();
+
+  res.redirect('/auth/login');
+});
+
+authRouter.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+authRouter.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/books',
+    failureRedirect: '/auth/login',
+    failureFlash: true
+  })
+);
+
+authRouter.get('/logout', (req, res) => {
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/auth/login');
+    });
+  
+});
+
+booksRouter.get('/', authenticate, async (req, res) => {
+  const books = [
+    { title: 'Book 1', author: 'Author 1' },
+    { title: 'Book 2', author: 'Author 2' },
+    // ... (add more books as needed)
+  ];
+
+  res.render('books', { books });
+});
+
+app.use('/auth', authRouter);
+app.use('/books', booksRouter);
+
+// Pug configuration
+app.set('view engine', 'pug');
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
